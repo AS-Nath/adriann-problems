b72533e54cb168d62862d9c3463a58583aa9524f
AS-Nath <aditya.nath.tech@gmail.com>
2026-01-28 12:26:43 +0530

    Q10

--- elem_q10.c
+++ elem_q10.c
@@ -2,7 +2,9 @@
 #include <stdbool.h>
 
 bool leap(int n) {
+    // if the year isn't divisible by 4, it's not a leap year. 
     if (n % 4 == 0) {
+        // if it is also divisible by 100, it must also be divisible by 400 to be a leap year.
         if (n % 100 == 0) {
             if (n % 400 == 0) {
                 return true;
@@ -21,6 +23,7 @@ bool leap(int n) {
 int main(void) {
     int year = 2026;
     int n = 0; 
+    // Loop indefinitely until we have counted 20 leap years. 
     while (true) {
         if (leap(year)) {
             printf("%d\n", year); 
